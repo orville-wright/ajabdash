a3c614ef8b196a992054e0d2074b88e9cac79364
orville-wright <orville.wright@yahoo.com>
2019-10-01 11:57:16 -0700

    Add ZERO alert URL

--- ajab_global_urls.py
+++ ajab_global_urls.py
@@ -15,6 +15,11 @@ PRINT_PHOME_1 = "?d=x&template=print"         # not sure of diff between these 2
 PRINT_PHOME_2 = "?template=print"             # not sure of diff between these 2
 PRINT_ATTEND = "?template=print_attendance"
 
+# Zero alert url
+#  https://ois-orinda-ca.schoolloop.com/student/zeros?d=x&return_url=1569955562286
+# I am unsure of the diff between the yet (at the moment
+ZERO_ALERT_0 = "student/zeros"
+ZERO_ALERT_1 = "student/zeros?d=x&return_url=1569955562286"
 
 # Progress report URL structure
 # https://ois-orinda-ca.schoolloop.com/progress_report/report?d=x&id=1500709128492&period_id=1563866703231&mark_id=current
